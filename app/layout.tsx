26a26936524b46cefc9aa16eaa8d50bdc115d9f4
Naveen S Bandara <116500705+S-A-N-D-A-R-U@users.noreply.github.com>
2024-10-14 09:42:07 +0530

    setup basic component structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type { Metadata } from "next"
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden ">{children}</main>
+        <Footer/>
       </body>
     </html>
   );
